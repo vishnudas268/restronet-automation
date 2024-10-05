12ea5358a8d4207c217b590452fbe9de5507de6f
Vkpixbit <vishnudas@pixbitsolutions.com>
2024-10-05 18:43:01 +0530

    login test completed

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,31 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
+function getLoginLocator(page){
+  return {
+    userName : page.locator("//input[@name='username']"),
+    password : page.locator("//input[@name='password']"),
+    loginButton : page.locator("//button[@name='login']")
+  };
+}
 
 
 test('login to restronet', async({page}) => {
   await page.goto('https://demo.restronet.com/newdemo/admin/login');
-  const userName = page.locator("//input[@name='username']");
-  const password = page.locator("//input[@name='password']")
+  const { userName, password, loginButton} = getLoginLocator(page)
   await userName.fill("administrator");
   await password.fill("123456");
+  await loginButton.click();
+  await page.waitForTimeout(6000); 
+  await expect(page).toHaveTitle("Restronet | Admin Dashboard :: Admin panel ") 
+})
+
+test('Branch Creation', async({page}) => {
+  await page.goto('https://demo.restronet.com/newdemo/admin/login');
+  const { userName, password, loginButton} = getLoginLocator(page)
+  await userName.fill("administrator");
+  await password.fill("123456");
+  await loginButton.click();
+  await page.waitForTimeout(6000); 
+  
 })
