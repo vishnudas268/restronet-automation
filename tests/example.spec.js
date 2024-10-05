d3dbb3cfcc940debd7419f861a88e566713c1b4b
Vkpixbit <vishnudas@pixbitsolutions.com>
2024-10-05 18:51:39 +0530

    branch creation started

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,6 +9,13 @@ function getLoginLocator(page){
   };
 }
 
+function getSideBarLocator(page){
+  return {
+    locationbar : page.locator("(//a[@class='has-arrow ai-icon '])[7]"),
+    branchManagement : page.locator("//a[text()=' Branch Management ']")
+  }
+}
+
 
 test('login to restronet', async({page}) => {
   await page.goto('https://demo.restronet.com/newdemo/admin/login');
@@ -23,9 +30,13 @@ test('login to restronet', async({page}) => {
 test('Branch Creation', async({page}) => {
   await page.goto('https://demo.restronet.com/newdemo/admin/login');
   const { userName, password, loginButton} = getLoginLocator(page)
+  const { locationbar, branchManagement} = getSideBarLocator(page)
   await userName.fill("administrator");
   await password.fill("123456");
   await loginButton.click();
   await page.waitForTimeout(6000); 
-  
+  await locationbar.click()
+  await branchManagement.click()
+  await page.waitForTimeout(6000); 
+  await expect(page).toHaveTitle("Restronet | View branches :: Admin panel ") 
 })
